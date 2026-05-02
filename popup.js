bacb34111ffba7825ccff82500c56c7101534ea4
Guilherme <gui.dias.alves20@gmail.com>
2026-05-02 01:08:49 +0100

    scroll + time interpretation + seek page + popup

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ const daysEl = document.getElementById("days");
 
 chrome.storage.local.get(["running", "maxDays"], (res) => {
   statusEl.textContent = res.running ? "Rodando" : "Parado";
-  if (res.maxDays) daysEl.value = res.maxDays;
+  if (typeof res.maxDays === "number") daysEl.value = res.maxDays;
 });
 
 document.getElementById("start").addEventListener("click", () => {
